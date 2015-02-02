43b71faf771fa57a4dd1f496c93862dbb6285544
blaze97531 <jrhansf@terpmail.umd.edu>
2015-02-02 17:24:40 -0500

    Client/Server code is established using socket IO. To run from windows command line, type
    node server.js
    
    You may need to install node.js to do so.
    
    Then your computer is running an http server that can be accessed through your web browser (e.g. http://localhost/)
    
    At the moment, all game logic is still done on the client-side. Audio has been temporarily removed.

---
+++ server.js
@@ -0,0 +1,41 @@
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+var fs = require('fs');
+
+var nextID = 0;
+
+app.get('/', function(req, res){
+	res.sendFile(__dirname + '\\client.html');
+});
+
+io.on('connection', function(socket){	
+	var id = nextID;
+	nextID = nextID + 1;
+	console.log(id + ' connected');
+	
+	socket.on('disconnect', function () {
+		console.log(id + ' disconnected'); 
+	});
+	
+	socket.on('request_image', function () {
+		fs.readFile(__dirname + '\\images\\coffee_shop.jpg', function(err, buf){
+			socket.emit('image', { image: 0, buffer: buf.toString('base64') });
+			console.log('Sending image');
+		});
+	
+		fs.readFile(__dirname + '\\images\\go_to_the_coffee_shop.jpg', function(err, buf){
+			socket.emit('image', { image: 1, buffer: buf.toString('base64') });
+			console.log('Sending image');
+		});
+	
+		fs.readFile(__dirname + '\\images\\inventory.png', function (err, buf){
+			socket.emit('image', { image: 2, buffer: buf.toString('base64') });
+			console.log('Sending image');
+		});
+	});
+});
+
+http.listen(80, function(){
+	console.log('Server started on port 80');
+});
