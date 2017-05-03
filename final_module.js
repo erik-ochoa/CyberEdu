b585f243ef4fa031ade663eb3708f01fbe381aa7
jiglesia3 <jiglesia3@gmail.com>
2017-05-03 11:44:13 -0400

    IP Changes for Final Module Router Portion

--- final_module.js
+++ final_module.js
@@ -23,14 +23,14 @@ function load_final_module (game) {
 		new Button ("router_cancel", 630, 342, 735, 390, 0)
 	], [], []);
 
-	game.webpages["http://192.168.0.1/wireless"] = new Screen (0, 70, 0, new Rectangle ("router_1_background", 0, 0, 1224, 688, 0, "rgba(255,255,255,1)"),
+	game.webpages["http://192.168.1.1/wireless"] = new Screen (0, 70, 0, new Rectangle ("router_1_background", 0, 0, 1224, 688, 0, "rgba(255,255,255,1)"),
 	[new Button ("final_change_ssid", 310, 210, 490, 240, 2, "Change SSID", "28px Arial", "rgba(0,0,0,1)"),
 	new Button ("final_set_pwd", 310, 260, 490, 290, 2, "Set Password", "28px Arial", "rgba(0,0,0,1)"),
 	new Button ("final_finish", 360, 400, 435, 430, 2, "Finish", "28px Arial", "rgba(0,0,0,1)")],
 	[],
 	[new Text ("router_name", 200, 55, 720, 85, 2,     "Elijah's Router (Unlocked/Open)")]);
 
-	game.webpages["http://192.168.0.1/wireless2"] = new Screen (0, 70, 0, new Rectangle ("router_1_background", 0, 0, 1224, 688, 0, "rgba(255,255,255,1)"),
+	game.webpages["http://192.168.1.1/wireless2"] = new Screen (0, 70, 0, new Rectangle ("router_1_background", 0, 0, 1224, 688, 0, "rgba(255,255,255,1)"),
 	[new Button ("final_change_ssid_2", 310, 210, 490, 240, 2, "Change SSID", "28px Arial", "rgba(0,0,0,1)"),
 	new Button ("final_set_pwd_2", 310, 260, 490, 290, 2, "Set Password", "28px Arial", "rgba(0,0,0,1)"),
 	new Button ("final_finish_2", 360, 400, 435, 430, 2, "Finish", "28px Arial", "rgba(0,0,0,1)")],
@@ -39,12 +39,12 @@ function load_final_module (game) {
 	//not sure how to reflect the newly changed SSID on the webpage
 	//[new Text ("router_name", 200, 55, 720, 85, 2,     game.final_modules_variables.ssid_entry)]);
 
-	game.webpages["http://192.168.0.1/wireless/ssid"] = new Screen (0, 70, 0, new Rectangle ("router_2_background", 0, 0, 1224, 688, 0, "rgba(255,255,255,1)"),
+	game.webpages["http://192.168.1.1/wireless/ssid"] = new Screen (0, 70, 0, new Rectangle ("router_2_background", 0, 0, 1224, 688, 0, "rgba(255,255,255,1)"),
 	[new Button ("final_ssid_finish", 100, 200, 200, 300, 2,"Save and Go Back >", "18px Arial", "rgba(0,0,0,1)")],
 	[new Button ("final_ssid_page_entry", 100, 130, 500, 200, 2, "********", "24px Arial", "rgba(64,64,64,1)")],
 	[new Text ("final_router_instruction_enter_ssid", 100, 100, 500, 150, 1, "Enter your new SSID:", "24px Arial", "rgba(0,0,0,1)")]);
 
-	game.webpages["http://192.168.0.1/wireless/password"] = new Screen (0, 70, 0, new Rectangle ("router_2_background", 0, 0, 1224, 688, 0, "rgba(255,255,255,1)"),
+	game.webpages["http://192.168.1.1/wireless/password"] = new Screen (0, 70, 0, new Rectangle ("router_2_background", 0, 0, 1224, 688, 0, "rgba(255,255,255,1)"),
 	[new Button ("final_router_password_finish", 100, 200, 200, 300, 2,"Save and Go Back >", "18px Arial", "rgba(0,0,0,1)")],
 	[new Button ("final_router_password_entry", 100, 130, 500, 200, 2, "********", "24px Arial", "rgba(64,64,64,1)")],
 	[new Text ("final_router_instruction_enter_password", 100, 100, 500, 150, 1, "Enter your new password:", "24px Arial", "rgba(0,0,0,1)")]);
@@ -78,7 +78,7 @@ function final_module_onclick (button, showDialog, closeDialog, changeMainScreen
 	} else if (button == "router_configure") {
 		if (!vars.configured_router) {
 			displayBrowser("final_browser");
-			changeBrowserWebPage(browser, "http://192.168.0.1/wireless");
+			changeBrowserWebPage(browser, "http://192.168.1.1/wireless");
 			return true;
 		} else {
 			return true;
@@ -88,7 +88,7 @@ function final_module_onclick (button, showDialog, closeDialog, changeMainScreen
 		return true;
 	} else if (button == "final_change_ssid" || button == "final_change_ssid_2") {
 		if (!vars.changed_ssid) {
-			changeBrowserWebPage(browser, "http://192.168.0.1/wireless/ssid");
+			changeBrowserWebPage(browser, "http://192.168.1.1/wireless/ssid");
 			return true;
 		} else {
 			return true;
@@ -96,10 +96,11 @@ function final_module_onclick (button, showDialog, closeDialog, changeMainScreen
 	} else if (button == "final_ssid_finish") {
 		if (vars.final_ssid_entry.length < 28) {
 			if (!vars.final_ssid_entry.includes("Elijah")) {
-				changeBrowserWebPage(browser, "http://192.168.0.1/wireless2");
+				changeBrowserWebPage(browser, "http://192.168.1.1/wireless2");
 				vars.changed_ssid = true;
 				return true;
 			} else {
+				showDialog("final_partner_dialog");
 				return true;
 			}
 		} else {
@@ -111,18 +112,18 @@ function final_module_onclick (button, showDialog, closeDialog, changeMainScreen
 		return true;
 	} else if (button == "final_set_pwd" || button == "final_set_pwd_2") {
 		if (!vars.changed_pwd) {
-			changeBrowserWebPage(browser, "http://192.168.0.1/wireless/password");
+			changeBrowserWebPage(browser, "http://192.168.1.1/wireless/password");
 			return true;
 		} else {
 			return true;
 		}
 	} else if (button == "final_router_password_finish") {
 		if (vars.changed_ssid) {
-			changeBrowserWebPage(browser, "http://192.168.0.1/wireless2");
+			changeBrowserWebPage(browser, "http://192.168.1.1/wireless2");
 			vars.changed_pwd = true;
 			return true;
 		} else {
-			changeBrowserWebPage(browser, "http://192.168.0.1/wireless");
+			changeBrowserWebPage(browser, "http://192.168.1.1/wireless");
 			vars.changed_pwd = true;
 			return true;
 		}
